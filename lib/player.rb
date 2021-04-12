cd8ef55ba8af20824c189a0501f2453ff46c3b1b
Rachael Aptowitz <raptowitz@gmail.com>
2021-04-11 21:07:02 -0700

    take and validate turn methods

--- lib/player.rb
+++ lib/player.rb
@@ -2,5 +2,21 @@
 
 # Players
 class Player
+  def initialize(token = 'X')
+    @token = token
+  end
 
+  def take_turn
+    puts "\nPlayer #{@token}, what is your move?"
+    move = gets.to_i
+    validate_turn(move)
+  end
+
+  def validate_turn(move)
+    until move >= 1 && move <= 7
+      puts 'Pick an unfilled column between 1-7!'
+      move = gets.to_i
+    end
+    move
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,28 +7,23 @@ describe Player do
     subject(:player) { described_class.new }
 
     context 'when player picks a valid column' do
-      before do
-        valid_input = 1
-        allow(player).to receive(:gets).and_return(valid_input)
-      end
-
       it 'stops loop and does not display instructions' do
         instructions = 'Pick an unfilled column between 1-7!'
         expect(player).not_to receive(:puts).with(instructions)
-        player.validate_turn
+        player.validate_turn(1)
       end
     end
 
-    context 'when player picks a valid column, then an invalid column' do
+    context 'when player picks an invalid column, then a valid column' do
       before do
         valid_input = 2
-        invalid_input = 9
-        allow(player).to receive(:gets).and_return(valid_input, invalid_input)
+        allow(player).to receive(:gets).and_return(valid_input)
       end
+
       it 'completes loop and display instructions once' do
         instructions = 'Pick an unfilled column between 1-7!'
         expect(player).to receive(:puts).with(instructions).once
-        player.validate_turn
+        player.validate_turn(9)
       end
     end
   end
